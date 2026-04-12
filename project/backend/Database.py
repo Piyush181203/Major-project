2412bdd3b649d8f801e853899177bf563742f4c8
Piyush181203 <piyushgole181203@gmail.com>
2026-04-12 18:44:45 +0530

    Create Database.py

---
+++ project/backend/Database.py
@@ -0,0 +1,18 @@
+import sqlite3
+from config import DB_PATH
+
+def get_db():
+    conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    db = get_db()
+    db.executescript("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    );
+    """)
+    db.commit()
